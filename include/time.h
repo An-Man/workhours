328b519537cc1e78308a3e607cc18d0a0d966fa4
An-Man <An-Man@github.com>
2023-09-09 20:03:00 +0300

    add rounding to nearest quarter

--- include/time.h
+++ include/time.h
@@ -16,11 +16,14 @@ class Workday {
         Time end {};
 
         void calculate_length();
-        void print_length();
+        void print_length_in_decimal();
+        void print_length_in_hh_mm();
 
     private:
 
-        float length;
+        float length_in_decimal;
+        Time length_in_hh_mm;
+        Time length_in_hh_mm_no_lunch;
 };
 
 Time ask_time(); 
--- src/time.cpp
+++ src/time.cpp
@@ -3,15 +3,32 @@
 
 void Workday::calculate_length() {
     int total_minutes {((end.hours * 60) + end.minutes) - ((start.hours * 60) + start.minutes)};
-    float total_hours {static_cast<float>(total_minutes) / 60.0f};
+
+    int total_hours {total_minutes/ 60};
+    float total_hours_f {static_cast<float>(total_minutes) / 60.0f}; // in decimal form
+
+    length_in_hh_mm.hours = total_hours;
+    length_in_hh_mm.minutes = total_minutes - (total_hours * 60);
     
-    // round to nearest quarter
-    // length = ;  
+    // multiply by 4 to get quarter hours
+    total_hours *= 4.0f;
+
+    // check which is nearest integer, floor or ceiling, and round to it. Ternary operator.
+    (total_hours - floorf(total_hours)) <= (ceilf(total_hours) - total_hours) ? 
+        total_hours = floorf(total_hours) : total_hours = ceilf(total_hours);
+
+    // divide by 4 to get actual value
+    length_in_decimal = total_hours / 4.0f; 
 }
 
-void Workday::print_length() {
-    std::cout << "Workday total: " << length << '\n';
-    std::cout << "Workday minus 30 min lunch: " << length - 0.50f << '\n'; 
+void Workday::print_length_in_decimal() {
+    std::cout << "Workday total: " << length_in_decimal << '\n';
+    std::cout << "Workday minus 30 min lunch: " << length_in_decimal - 0.50f << '\n';
+}
+
+void Workday::print_length_in_hh_mm() {
+    std::cout << "Workday total: " << length_in_hh_mm.hours << " h " << length_in_hh_mm.minutes << "min\n";
+    std::cout << "Workday minus 30 min lunch: " << length_in_hh_mm_no_lunch.hours << " h " << length_in_hh_mm_no_lunch.minutes << "min\n"; 
 }
 
 Time ask_time() {
