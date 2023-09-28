729b96c3bdc86d9fb90fc28fb67844dbaf54fb94
An-Man <An-Man@github.com>
2023-09-28 22:57:00 +0300

    add gif, testing out stuff

--- workhours_ui/build-workhours_ui-Desktop_Qt_5_15_15_GCC_64bit-Debug/workhours_ui_autogen/include/ui_mainwindow.h
+++ workhours_ui/build-workhours_ui-Desktop_Qt_5_15_15_GCC_64bit-Debug/workhours_ui_autogen/include/ui_mainwindow.h
@@ -12,7 +12,6 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QFrame>
-#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QLCDNumber>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
@@ -28,18 +27,6 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QGridLayout *gridLayout;
-    QLabel *label;
-    QLineEdit *lineEdit_start_hh;
-    QLabel *label_3;
-    QLineEdit *lineEdit_start_mm;
-    QLabel *label_5;
-    QLabel *label_2;
-    QPushButton *pushButton;
-    QLineEdit *lineEdit_end_hh;
-    QLabel *label_7;
-    QLineEdit *lineEdit_end_mm;
-    QLabel *label_8;
     QFrame *line;
     QLabel *label_4;
     QLCDNumber *lcdNumber_length_total_hh;
@@ -55,6 +42,18 @@ public:
     QLabel *label_11;
     QLCDNumber *lcdNumber_length_no_lunch_dec;
     QLabel *label_14;
+    QLabel *label;
+    QLineEdit *lineEdit_start_hh;
+    QLineEdit *lineEdit_start_mm;
+    QLabel *label_2;
+    QLineEdit *lineEdit_end_hh;
+    QLabel *label_7;
+    QLabel *label_3;
+    QLineEdit *lineEdit_end_mm;
+    QLabel *label_5;
+    QLabel *label_8;
+    QPushButton *pushButton;
+    QLabel *gif_label;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -62,153 +61,107 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(371, 382);
+        MainWindow->resize(417, 382);
+        MainWindow->setAutoFillBackground(false);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-        gridLayout = new QGridLayout(centralwidget);
-        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
-        label = new QLabel(centralwidget);
-        label->setObjectName(QString::fromUtf8("label"));
-
-        gridLayout->addWidget(label, 0, 0, 1, 1);
-
-        lineEdit_start_hh = new QLineEdit(centralwidget);
-        lineEdit_start_hh->setObjectName(QString::fromUtf8("lineEdit_start_hh"));
-
-        gridLayout->addWidget(lineEdit_start_hh, 1, 0, 1, 2);
-
-        label_3 = new QLabel(centralwidget);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
-
-        gridLayout->addWidget(label_3, 1, 2, 1, 1);
-
-        lineEdit_start_mm = new QLineEdit(centralwidget);
-        lineEdit_start_mm->setObjectName(QString::fromUtf8("lineEdit_start_mm"));
-
-        gridLayout->addWidget(lineEdit_start_mm, 1, 3, 1, 3);
-
-        label_5 = new QLabel(centralwidget);
-        label_5->setObjectName(QString::fromUtf8("label_5"));
-
-        gridLayout->addWidget(label_5, 1, 6, 1, 1);
-
-        label_2 = new QLabel(centralwidget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-
-        gridLayout->addWidget(label_2, 2, 0, 1, 1);
-
-        pushButton = new QPushButton(centralwidget);
-        pushButton->setObjectName(QString::fromUtf8("pushButton"));
-
-        gridLayout->addWidget(pushButton, 2, 7, 1, 2);
-
-        lineEdit_end_hh = new QLineEdit(centralwidget);
-        lineEdit_end_hh->setObjectName(QString::fromUtf8("lineEdit_end_hh"));
-
-        gridLayout->addWidget(lineEdit_end_hh, 3, 0, 1, 2);
-
-        label_7 = new QLabel(centralwidget);
-        label_7->setObjectName(QString::fromUtf8("label_7"));
-
-        gridLayout->addWidget(label_7, 3, 2, 1, 1);
-
-        lineEdit_end_mm = new QLineEdit(centralwidget);
-        lineEdit_end_mm->setObjectName(QString::fromUtf8("lineEdit_end_mm"));
-
-        gridLayout->addWidget(lineEdit_end_mm, 3, 3, 1, 3);
-
-        label_8 = new QLabel(centralwidget);
-        label_8->setObjectName(QString::fromUtf8("label_8"));
-
-        gridLayout->addWidget(label_8, 3, 6, 1, 1);
-
         line = new QFrame(centralwidget);
         line->setObjectName(QString::fromUtf8("line"));
+        line->setGeometry(QRect(10, 150, 399, 16));
         line->setFrameShape(QFrame::HLine);
         line->setFrameShadow(QFrame::Sunken);
-
-        gridLayout->addWidget(line, 4, 0, 1, 9);
-
         label_4 = new QLabel(centralwidget);
         label_4->setObjectName(QString::fromUtf8("label_4"));
-
-        gridLayout->addWidget(label_4, 5, 0, 1, 3);
-
+        label_4->setGeometry(QRect(9, 176, 110, 17));
         lcdNumber_length_total_hh = new QLCDNumber(centralwidget);
         lcdNumber_length_total_hh->setObjectName(QString::fromUtf8("lcdNumber_length_total_hh"));
+        lcdNumber_length_total_hh->setGeometry(QRect(9, 216, 37, 23));
         lcdNumber_length_total_hh->setDigitCount(2);
-
-        gridLayout->addWidget(lcdNumber_length_total_hh, 6, 0, 1, 1);
-
         label_10 = new QLabel(centralwidget);
         label_10->setObjectName(QString::fromUtf8("label_10"));
-
-        gridLayout->addWidget(label_10, 6, 1, 1, 1);
-
+        label_10->setGeometry(QRect(111, 216, 18, 17));
         lcdNumber_length_total_mm = new QLCDNumber(centralwidget);
         lcdNumber_length_total_mm->setObjectName(QString::fromUtf8("lcdNumber_length_total_mm"));
+        lcdNumber_length_total_mm->setGeometry(QRect(135, 216, 37, 23));
         lcdNumber_length_total_mm->setDigitCount(2);
-
-        gridLayout->addWidget(lcdNumber_length_total_mm, 6, 2, 1, 2);
-
         label_9 = new QLabel(centralwidget);
         label_9->setObjectName(QString::fromUtf8("label_9"));
-
-        gridLayout->addWidget(label_9, 6, 4, 1, 1);
-
+        label_9->setGeometry(QRect(237, 216, 26, 17));
         lcdNumber_length_total_dec = new QLCDNumber(centralwidget);
         lcdNumber_length_total_dec->setObjectName(QString::fromUtf8("lcdNumber_length_total_dec"));
+        lcdNumber_length_total_dec->setGeometry(QRect(269, 216, 55, 23));
         lcdNumber_length_total_dec->setDigitCount(4);
-
-        gridLayout->addWidget(lcdNumber_length_total_dec, 6, 5, 1, 3);
-
         label_13 = new QLabel(centralwidget);
         label_13->setObjectName(QString::fromUtf8("label_13"));
-
-        gridLayout->addWidget(label_13, 6, 8, 1, 1);
-
+        label_13->setGeometry(QRect(355, 216, 53, 17));
         label_6 = new QLabel(centralwidget);
         label_6->setObjectName(QString::fromUtf8("label_6"));
-
-        gridLayout->addWidget(label_6, 7, 0, 1, 4);
-
+        label_6->setGeometry(QRect(9, 255, 171, 17));
         lcdNumber_length_no_lunch_hh = new QLCDNumber(centralwidget);
         lcdNumber_length_no_lunch_hh->setObjectName(QString::fromUtf8("lcdNumber_length_no_lunch_hh"));
+        lcdNumber_length_no_lunch_hh->setGeometry(QRect(9, 295, 37, 23));
         lcdNumber_length_no_lunch_hh->setDigitCount(2);
-
-        gridLayout->addWidget(lcdNumber_length_no_lunch_hh, 8, 0, 1, 1);
-
         label_12 = new QLabel(centralwidget);
         label_12->setObjectName(QString::fromUtf8("label_12"));
-
-        gridLayout->addWidget(label_12, 8, 1, 1, 1);
-
+        label_12->setGeometry(QRect(111, 295, 18, 17));
         lcdNumber_length_no_lunch_mm = new QLCDNumber(centralwidget);
         lcdNumber_length_no_lunch_mm->setObjectName(QString::fromUtf8("lcdNumber_length_no_lunch_mm"));
+        lcdNumber_length_no_lunch_mm->setGeometry(QRect(135, 295, 37, 23));
         lcdNumber_length_no_lunch_mm->setDigitCount(2);
-
-        gridLayout->addWidget(lcdNumber_length_no_lunch_mm, 8, 2, 1, 2);
-
         label_11 = new QLabel(centralwidget);
         label_11->setObjectName(QString::fromUtf8("label_11"));
-
-        gridLayout->addWidget(label_11, 8, 4, 1, 1);
-
+        label_11->setGeometry(QRect(237, 295, 26, 17));
         lcdNumber_length_no_lunch_dec = new QLCDNumber(centralwidget);
         lcdNumber_length_no_lunch_dec->setObjectName(QString::fromUtf8("lcdNumber_length_no_lunch_dec"));
+        lcdNumber_length_no_lunch_dec->setGeometry(QRect(269, 295, 55, 23));
         lcdNumber_length_no_lunch_dec->setDigitCount(4);
-
-        gridLayout->addWidget(lcdNumber_length_no_lunch_dec, 8, 5, 1, 3);
-
         label_14 = new QLabel(centralwidget);
         label_14->setObjectName(QString::fromUtf8("label_14"));
-
-        gridLayout->addWidget(label_14, 8, 8, 1, 1);
-
+        label_14->setGeometry(QRect(355, 295, 53, 17));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(9, 9, 69, 17));
+        lineEdit_start_hh = new QLineEdit(centralwidget);
+        lineEdit_start_hh->setObjectName(QString::fromUtf8("lineEdit_start_hh"));
+        lineEdit_start_hh->setGeometry(QRect(2, 39, 96, 25));
+        lineEdit_start_mm = new QLineEdit(centralwidget);
+        lineEdit_start_mm->setObjectName(QString::fromUtf8("lineEdit_start_mm"));
+        lineEdit_start_mm->setGeometry(QRect(128, 39, 96, 25));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(2, 70, 61, 17));
+        lineEdit_end_hh = new QLineEdit(centralwidget);
+        lineEdit_end_hh->setObjectName(QString::fromUtf8("lineEdit_end_hh"));
+        lineEdit_end_hh->setGeometry(QRect(2, 110, 96, 25));
+        label_7 = new QLabel(centralwidget);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+        label_7->setGeometry(QRect(104, 39, 18, 17));
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(104, 110, 18, 17));
+        lineEdit_end_mm = new QLineEdit(centralwidget);
+        lineEdit_end_mm->setObjectName(QString::fromUtf8("lineEdit_end_mm"));
+        lineEdit_end_mm->setGeometry(QRect(128, 110, 96, 25));
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(230, 39, 26, 17));
+        label_8 = new QLabel(centralwidget);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+        label_8->setGeometry(QRect(230, 110, 26, 17));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(270, 100, 111, 41));
+        gif_label = new QLabel(centralwidget);
+        gif_label->setObjectName(QString::fromUtf8("gif_label"));
+        gif_label->setGeometry(QRect(290, 20, 61, 61));
+        gif_label->setMinimumSize(QSize(50, 50));
+        gif_label->setPixmap(QPixmap(QString::fromUtf8(":/fonts/piis_iso.gif")));
+        gif_label->setScaledContents(true);
+        gif_label->setAlignment(Qt::AlignCenter);
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 371, 22));
+        menubar->setGeometry(QRect(0, 0, 417, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -222,13 +175,6 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "workhours", nullptr));
-        label->setText(QCoreApplication::translate("MainWindow", "Start time", nullptr));
-        label_3->setText(QCoreApplication::translate("MainWindow", "hh", nullptr));
-        label_5->setText(QCoreApplication::translate("MainWindow", "mm", nullptr));
-        label_2->setText(QCoreApplication::translate("MainWindow", "End time", nullptr));
-        pushButton->setText(QCoreApplication::translate("MainWindow", "Calculate", nullptr));
-        label_7->setText(QCoreApplication::translate("MainWindow", "hh", nullptr));
-        label_8->setText(QCoreApplication::translate("MainWindow", "mm", nullptr));
         label_4->setText(QCoreApplication::translate("MainWindow", "Day length total", nullptr));
         label_10->setText(QCoreApplication::translate("MainWindow", "hh", nullptr));
         label_9->setText(QCoreApplication::translate("MainWindow", "mm", nullptr));
@@ -237,6 +183,14 @@ public:
         label_12->setText(QCoreApplication::translate("MainWindow", "hh", nullptr));
         label_11->setText(QCoreApplication::translate("MainWindow", "mm", nullptr));
         label_14->setText(QCoreApplication::translate("MainWindow", "h in dec", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Start time", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "End time", nullptr));
+        label_7->setText(QCoreApplication::translate("MainWindow", "hh", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "hh", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "mm", nullptr));
+        label_8->setText(QCoreApplication::translate("MainWindow", "mm", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Calculate", nullptr));
+        gif_label->setText(QString());
     } // retranslateUi
 
 };
--- workhours_ui/main.cpp
+++ workhours_ui/main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QMovie>
 
 int main(int argc, char *argv[])
 {
